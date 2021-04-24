b6bf338b3b053a9999f4a1f735405ff47beb953f
monsieurluge <monsieurluge@gmail.com>
2021-04-24 17:44:28 +0200

    browser "back" shortcut

--- keyboards/crkbd/keymaps/monsieurluge/keymap.c
+++ keyboards/crkbd/keymaps/monsieurluge/keymap.c
@@ -27,6 +27,7 @@ extern uint8_t is_master;
 #define COPY    LCTL(KC_C)
 #define PASTE   LCTL(KC_V)
 #define UNDO    LCTL(KC_Z)
+#define BACK    LALT(KC_LEFT)        // browser "back" shortcut
 #define SP_GAME TG(LY_GAME)          // toggles the "gaming" layer
 #define SP_CTTB CTL_T(KC_TAB)        // tab or left control when held
 #define SP_CTEN CTL_T(KC_ENT)        // enter or left control when held
@@ -90,7 +91,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ├─────┼─────┤  =  ├─────┤  {  ├─────┤              ├─────┤  }  ├─────┤  _  ├─────┼─────┤
      * │     │  '  ├─────┤copy ├─────┤  "  │              │  |  ├─────┤  €  ├─────┤  `  │     │
      * ├─────┼─────┤ cut ├─────┤  [  ├─────┤              ├─────┤  ]  ├─────┤     ├─────┼─────┤
-     * │menu │undo ├─────┘     └─────┤paste│              │     ├─────┘     └─────┤  \  │     │
+     * │menu │undo ├─────┘     └─────┤paste│              │back ├─────┘     └─────┤  \  │     │
      * └─────┴─────┘        ┌─────┐  └─────┘              └─────┘  ┌─────┐        └─────┴─────┘
      *                      │     │┌─────┐┌─────┐    ┌─────┐┌─────┐│     │
      *                      └─────┘│     ││     │    │     ││     │└─────┘
@@ -99,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LY_FN1] = LAYOUT( \
     KC_TILD, KC_EXLM, KC_AT,    KC_HASH, KC_DLR,  KC_PERC,  KC_CIRC, KC_AMPR, KC_ASTR,    KC_LPRN, KC_RPRN, KC_DEL, \
     _______, KC_QUOT, KC_EQUAL, KC_PLUS, KC_LCBR, KC_DQT,   KC_PIPE, KC_RCBR, KC_MINS,    KC_UNDS, KC_GRV,  _______, \
-    KC_APP,  KC_UNDO, KC_CUT,   KC_COPY, KC_LBRC, KC_PASTE, XXXXXXX, KC_RBRC, RALT(KC_5), XXXXXXX, KC_BSLS, _______, \
+    KC_APP,  KC_UNDO, KC_CUT,   KC_COPY, KC_LBRC, KC_PASTE, BACK,    KC_RBRC, RALT(KC_5), XXXXXXX, KC_BSLS, _______, \
                                 _______, _______, _______,  _______, _______, _______ \
     ),
 
