19a5e4a9ee6b02201041dfd2e3b6c6750b687710
monsieurluge <monsieurluge@gmail.com>
2021-11-27 14:50:33 +0100

    corne: fix edit keys

--- keyboards/crkbd/keymaps/monsieurluge/keymap.c
+++ keyboards/crkbd/keymaps/monsieurluge/keymap.c
@@ -89,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LY_FN1] = LAYOUT_split_3x6_3(
         KC_TILD, KC_EXLM, KC_AT,    KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR,    KC_LPRN, KC_RPRN, KC_DEL,
         _______, KC_QUOT, KC_EQUAL, KC_PLUS, KC_LCBR, KC_DQT,  KC_PIPE, KC_RCBR, KC_MINS,    KC_UNDS, KC_GRV,  _______,
-        KC_APP,  KC_UNDO, KC_CUT,   KC_COPY, KC_LBRC, KC_PAST, XXXXXXX, KC_RBRC, RALT(KC_5), XXXXXXX, KC_BSLS, _______,
+        KC_APP,  UNDO___, CUT____,  COPY___, KC_LBRC, PASTE__, XXXXXXX, KC_RBRC, RALT(KC_5), XXXXXXX, KC_BSLS, _______,
                                     _______, _______, _______, _______, _______, _______
     ),
 
