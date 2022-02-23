6d8c5b2bf5bfa54115c0a0a3f01e05302b0558e5
monsieurluge <monsieurluge@gmail.com>
2022-02-23 22:44:11 +0100

    planck: shift some shortcuts

--- keyboards/planck/keymaps/monsieurluge/keymap.c
+++ keyboards/planck/keymaps/monsieurluge/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
      * │shift│  Z  │  X  │  C  │  D  │  V  │  K  │  H  │  ,  │  .  │  /  │shift│
      * ├─────┼─────┼─────┼─────┼─────┼─────┴─────┼─────┼─────┼─────┼─────┼─────┤
-     * │ctrl │ alt │ gui │ fn3 │ fn1 │ space/ctl │ fn2 │     │ gui │ alt │ctrl │
+     * │ctrl │ alt │ gui │ fn3 │ fn1 │ space/ctl │ fn2 │ alt │     │ alt │ctrl │
      * └─────┴─────┴─────┴─────┴─────┴───────────┴─────┴─────┴─────┴─────┴─────┘
      */
 
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,   KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC, \
     KC_TAB,   KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_ENT, \
     KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-    KC_LCTRL, KC_LALT, KC_LGUI, SP_LY03, SP_LY01, CTSP___, CTSP___, SP_LY02, XXXXXXX, KC_LGUI, KC_LALT, KC_RCTL \
+    KC_LCTRL, KC_LALT, KC_LGUI, SP_LY03, SP_LY01, CTSP___, CTSP___, SP_LY02, KC_LALT, XXXXXXX, KC_LALT, KC_RCTL \
     ),
 
     /* fn1 - symbols
@@ -47,21 +47,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
      * │     │     │     │     │  [  │  /  │  \  │  ]  │  €  │     │  \  │     │
      * ├─────┼─────┼─────┼─────┼─────┼─────┴─────┼─────┼─────┼─────┼─────┼─────┤
-     * │     │     │menu │     │     │           │     │     │menu │     │     │
+     * │     │     │menu │     │     │           │     │     │     │     │     │
      * └─────┴─────┴─────┴─────┴─────┴───────────┴─────┴─────┴─────┴─────┴─────┘
      */
     [LY_FN1] = LAYOUT_planck_grid( \
     KC_TILD, KC_EXLM, KC_AT,    KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL, \
     _______, KC_QUOT, KC_EQUAL, KC_PLUS, KC_LCBR, KC_DQT,  KC_PIPE, KC_RCBR, KC_MINS, KC_UNDS, KC_GRV,  _______, \
     _______, XXXXXXX, XXXXXXX,  XXXXXXX, KC_LBRC, KC_SLSH, KC_BSLS, KC_RBRC, SP_EURO, XXXXXXX, KC_BSLS, _______, \
-    _______, _______, KC_APP,   _______, _______, _______, _______, _______, _______, KC_APP,  _______, _______ \
+    _______, _______, KC_APP,   _______, _______, _______, _______, _______, _______, _______, _______, _______ \
     ),
 
     /* fn2 - navigation and function keys
      * ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
      * │ F1  │ F2  │ F3  │ F4  │ F5  │ F6  │ F7  │ F8  │ F9  │ F10 │ F11 │ F12 │
      * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
-     * │     │     │     │     │ctrl │     │home │left │down │ up  │right│     │
+     * │     │     │     │shift│ctrl │     │home │left │down │ up  │right│     │
      * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
      * │     │     │     │     │ptscr│     │back │ end │pgdwn│pgup │     │     │
      * ├─────┼─────┼─────┼─────┼─────┼─────┴─────┼─────┼─────┼─────┼─────┼─────┤
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [LY_FN2] = LAYOUT_planck_grid( \
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
-    _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_LCTRL, XXXXXXX, KC_HOME, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, \
+    _______, XXXXXXX, XXXXXXX, KC_LSFT, KC_LCTRL, XXXXXXX, KC_HOME, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, \
     _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR,  XXXXXXX, BACK___, KC_END,  KC_PGDN, KC_PGUP, XXXXXXX, _______, \
     _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______ \
     ),
