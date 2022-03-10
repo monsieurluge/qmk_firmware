d609a5d1d3f14480bd4955996e94b8139697cab7
monsieurluge <monsieurluge@gmail.com>
2022-03-10 20:40:04 +0100

    crkbd: replace the right ctrl key by alt, add shift to the nav layer

--- keyboards/crkbd/keymaps/monsieurluge/keymap.c
+++ keyboards/crkbd/keymaps/monsieurluge/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ├─────┼─────┤  X  ├─────┤  D  ├─────┤              ├─────┤  H  ├─────┤  .  ├─────┼─────┤
      * │ gui │  Z  ├─────┘     └─────┤  V  │              │  K  ├─────┘     └─────┤  /  │ alt │
      * └─────┴─────┘        ┌─────┐  └─────┘              └─────┘  ┌─────┐        └─────┴─────┘
-     *                      │ fn3 │┌─────┐┌─────┐    ┌─────┐┌─────┐│ctrl │
+     *                      │ fn3 │┌─────┐┌─────┐    ┌─────┐┌─────┐│ alt │
      *                      └─────┘│ fn1 ││shift│    │sp/ct││ fn2 │└─────┘
      *                             └─────┘└─────┘    └─────┘└─────┘
      */
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_Q, KC_W, KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,   KC_SCLN, KC_BSPC,
         KC_TAB,  KC_A, KC_R, KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,   KC_O,    KC_ENT,
         KC_LGUI, KC_Z, KC_X, KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT, KC_SLSH, KC_LALT,
-                             LY03___, LY01___, KC_LSFT, CTSP___, LY02___, KC_LCTRL
+                             LY03___, LY01___, KC_LSFT, CTSP___, LY02___, KC_LALT
     ),
 
     /* gaming "layer mask"
@@ -143,7 +143,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                   ┌─────┐                                      ┌─────┐
      *             ┌─────┤ F4  ├─────┐                          ┌─────┤ F9  ├─────┐
      * ┌─────┬─────┤ F3  ├─────┤ F5  ├─────┐              ┌─────┤ F8  ├─────┤ F10 ├─────┬─────┐
-     * │ F1  │ F2  ├─────┤     ├─────┤ F6  │              │ F7  ├─────┤down ├─────┤ F11 │ F12 │
+     * │ F1  │ F2  ├─────┤shift├─────┤ F6  │              │ F7  ├─────┤down ├─────┤ F11 │ F12 │
      * ├─────┼─────┤     ├─────┤ctrl ├─────┤              ├─────┤left ├─────┤ up  ├─────┼─────┤
      * │     │     ├─────┤     ├─────┤     │              │home ├─────┤pgdwn├─────┤right│     │
      * ├─────┼─────┤     ├─────┤     ├─────┤              ├─────┤ end ├─────┤pgup ├─────┼─────┤
@@ -154,10 +154,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                             └─────┘└─────┘    └─────┘└─────┘
      */
     [LY_FN2] = LAYOUT_split_3x6_3(
-        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,      KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-        _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_LCTRL, XXXXXXX,    KC_HOME, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
-        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_PSCREEN, BACK___, KC_END,  KC_PGDN, KC_PGUP, XXXXXXX, _______,
-                                   _______, _______,  _______,    _______, _______, _______
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,     KC_F5,    KC_F6,      KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+        _______, XXXXXXX, XXXXXXX, KC_LSHIFT, KC_LCTRL, XXXXXXX,    KC_HOME, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX,  KC_PSCREEN, BACK___, KC_END,  KC_PGDN, KC_PGUP, XXXXXXX, _______,
+                                   _______,   _______,  _______,    _______, _______, _______
     ),
 
     /* fn3 - numbers and media
