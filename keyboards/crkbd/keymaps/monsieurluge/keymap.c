d2cc1cd63d662d59f555ddc7deb142ea71127095
monsieurluge <monsieurluge@gmail.com>
2022-11-15 13:20:48 +0100

    crkbd(gaming layer): swap the space key, restore the fn1 modifier

--- keyboards/crkbd/keymaps/monsieurluge/keymap.c
+++ keyboards/crkbd/keymaps/monsieurluge/keymap.c
@@ -54,14 +54,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │shift│  Z  ├─────┘     └─────┤  B  │              │  N  ├─────┘     └─────┤  /  │shift│
      * └─────┴─────┘        ┌─────┐  └─────┘              └─────┘  ┌─────┐        └─────┴─────┘
      *                      │ ctl │┌─────┐┌─────┐    ┌─────┐┌─────┐│ gui │
-     *                      └─────┘│ alt ││space│    │sp/ct││ fn2 │└─────┘
+     *                      └─────┘│space││ fn1 │    │sp/ct││ fn2 │└─────┘
      *                             └─────┘└─────┘    └─────┘└─────┘
      */
     [LY_GAME] = LAYOUT_split_3x6_3(
-        KC_ESC,    KC_Q, KC_W, KC_E,  KC_R,    KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_BSPC,
-        KC_TAB,    KC_A, KC_S, KC_D,  KC_F,    KC_G,   KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_ENT,
-        KC_LSHIFT, KC_Z, KC_X, KC_C,  KC_V,    KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_LSHIFT,
-                             KC_LCTL, KC_LALT, KC_SPC, CTSP___, LY02___, KC_RGUI
+        KC_ESC,    KC_Q, KC_W, KC_E,  KC_R,   KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_BSPC,
+        KC_TAB,    KC_A, KC_S, KC_D,  KC_F,   KC_G,    KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_ENT,
+        KC_LSHIFT, KC_Z, KC_X, KC_C,  KC_V,   KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_LSHIFT,
+                             KC_LCTL, KC_SPC, LY01___, CTSP___, LY02___, KC_RGUI
     ),
 
     /* fn1 - symbols
