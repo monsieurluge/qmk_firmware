97d5e96e2cc5ea9210cd5cf1b53930648094ac9f
monsieurluge <monsieurluge@gmail.com>
2022-11-15 13:10:38 +0100

    crkbd: replace the left GUI key by left ALT
    crkbd: replace the left GUI key by left ALT

--- keyboards/crkbd/keymaps/monsieurluge/keymap.c
+++ keyboards/crkbd/keymaps/monsieurluge/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ├─────┼─────┤  X  ├─────┤  V  ├─────┤              ├─────┤  M  ├─────┤  .  ├─────┼─────┤
      * │shift│  Z  ├─────┘     └─────┤  B  │              │  N  ├─────┘     └─────┤  /  │shift│
      * └─────┴─────┘        ┌─────┐  └─────┘              └─────┘  ┌─────┐        └─────┴─────┘
-     *                      │ gui │┌─────┐┌─────┐    ┌─────┐┌─────┐│ gui │
+     *                      │ alt │┌─────┐┌─────┐    ┌─────┐┌─────┐│ gui │
      *                      └─────┘│ fn3 ││ fn1 │    │sp/ct││ fn2 │└─────┘
      *                             └─────┘└─────┘    └─────┘└─────┘
      */
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,    KC_Q, KC_W, KC_E,  KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_BSPC,
         KC_TAB,    KC_A, KC_S, KC_D,  KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_ENT,
         KC_LSHIFT, KC_Z, KC_X, KC_C,  KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_LSHIFT,
-                             KC_LGUI, LY03___, LY01___, CTSP___, LY02___, KC_RGUI
+                             KC_LALT, LY03___, LY01___, CTSP___, LY02___, KC_RGUI
     ),
 
     /* gaming layer
