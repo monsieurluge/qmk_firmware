f5c34aa20476df67afd29d91e8e2c8135c2a4984
Laurent Vergé <l.verge@vitrinemedia.com>
2022-12-07 14:44:03 +0100

    crkbd: move the shift & alt keys

--- keyboards/crkbd/keymaps/monsieurluge_right/keymap.c
+++ keyboards/crkbd/keymaps/monsieurluge_right/keymap.c
@@ -30,17 +30,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ├─────┼─────┤  S  ├─────┤  F  ├─────┤              ├─────┤  J  ├─────┤  L  ├─────┼─────┤
      * │ tab │  A  ├─────┤  C  ├─────┤  G  │              │  H  ├─────┤  ,  ├─────┤  ;  │enter│
      * ├─────┼─────┤  X  ├─────┤  V  ├─────┤              ├─────┤  M  ├─────┤  .  ├─────┼─────┤
-     * │shift│  Z  ├─────┘     └─────┤  B  │              │  N  ├─────┘     └─────┤  /  │shift│
+     * │shift│  Z  ├─────┘     └─────┤  B  │              │  N  ├─────┘     └─────┤  /  │ alt │
      * └─────┴─────┘        ┌─────┐  └─────┘              └─────┘  ┌─────┐        └─────┴─────┘
-     *                      │ alt │┌─────┐┌─────┐    ┌─────┐┌─────┐│ gui │
-     *                      └─────┘│ fn3 ││ fn1 │    │sp/ct││ fn2 │└─────┘
+     *                      │ fn3 │┌─────┐┌─────┐    ┌─────┐┌─────┐│ gui │
+     *                      └─────┘│shift││ fn1 │    │sp/ct││ fn2 │└─────┘
      *                             └─────┘└─────┘    └─────┘└─────┘
      */
     [LY_BASE] = LAYOUT_split_3x6_3(
-        KC_ESC,    KC_Q, KC_W, KC_E,  KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_BSPC,
-        KC_TAB,    KC_A, KC_S, KC_D,  KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_ENT,
-        KC_LSHIFT, KC_Z, KC_X, KC_C,  KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_LSHIFT,
-                             KC_LALT, LY03___, LY01___, CTSP___, LY02___, KC_RGUI
+        KC_ESC,    KC_Q, KC_W, KC_E,  KC_R,      KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_BSPC,
+        KC_TAB,    KC_A, KC_S, KC_D,  KC_F,      KC_G,    KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_ENT,
+        KC_LSHIFT, KC_Z, KC_X, KC_C,  KC_V,      KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RALT,
+                             LY03___, KC_LSHIFT, LY01___, CTSP___, LY02___, KC_RGUI
     ),
 
     /* gaming layer
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ├─────┼─────┤  S  ├─────┤  F  ├─────┤              ├─────┤  J  ├─────┤  L  ├─────┼─────┤
      * │ tab │  A  ├─────┤  C  ├─────┤  G  │              │  H  ├─────┤  ,  ├─────┤  ;  │enter│
      * ├─────┼─────┤  X  ├─────┤  V  ├─────┤              ├─────┤  M  ├─────┤  .  ├─────┼─────┤
-     * │shift│  Z  ├─────┘     └─────┤  B  │              │  N  ├─────┘     └─────┤  /  │shift│
+     * │shift│  Z  ├─────┘     └─────┤  B  │              │  N  ├─────┘     └─────┤  /  │ alt │
      * └─────┴─────┘        ┌─────┐  └─────┘              └─────┘  ┌─────┐        └─────┴─────┘
      *                      │ ctl │┌─────┐┌─────┐    ┌─────┐┌─────┐│ gui │
      *                      └─────┘│space││ fn1 │    │sp/ct││ fn2 │└─────┘
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LY_GAME] = LAYOUT_split_3x6_3(
         KC_ESC,    KC_Q, KC_W, KC_E,  KC_R,   KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_BSPC,
         KC_TAB,    KC_A, KC_S, KC_D,  KC_F,   KC_G,    KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_ENT,
-        KC_LSHIFT, KC_Z, KC_X, KC_C,  KC_V,   KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_LSHIFT,
+        KC_LSHIFT, KC_Z, KC_X, KC_C,  KC_V,   KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RALT,
                              KC_LCTL, KC_SPC, LY01___, CTSP___, LY02___, KC_RGUI
     ),
 
